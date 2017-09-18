7f124025a3a99d931f11784f2b586177c5e86a01
vishalpachori <vishalpachori25@hotmail.com>
2017-09-18 15:59:54 +0530

    UI changes in Detail page

--- PlanMyTrips/Models/CityToVisit.cs
+++ PlanMyTrips/Models/CityToVisit.cs
@@ -2,24 +2,24 @@
 
 namespace PlanMyTrips
 {
-	public class CityToVisit
-	{
-		[JsonProperty(PropertyName = "id")]
-		public string Id { get; set; }
+    public class CityToVisit
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
 
-		[JsonProperty(PropertyName = "name")]
-		public string Name { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
 
-		[JsonProperty(PropertyName = "planneddays")]
-        public int PlannedDays { get; set; }
+        [JsonProperty(PropertyName = "planneddays")]
+        public string PlannedDays { get; set; }
 
-		[JsonProperty(PropertyName = "actualdays")]
-		public int ActualDays { get; set; }
+        [JsonProperty(PropertyName = "actualdays")]
+        public int ActualDays { get; set; }
 
-		[JsonProperty(PropertyName = "visited")]
-		public bool Visited { get; set; }
+        [JsonProperty(PropertyName = "visited")]
+        public bool Visited { get; set; }
 
-		[JsonProperty(PropertyName = "notes")]
-		public string Notes { get; set; }
-	}
+        [JsonProperty(PropertyName = "notes")]
+        public string Notes { get; set; }
+    }
 }
